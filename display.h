0a049206ab32e26885a7aaa7a52890745ad94300
ATFox <99324602+atfox272@users.noreply.github.com>
2022-12-03 19:08:47 +0700

    Update display.h

--- display.h
+++ display.h
@@ -6,8 +6,7 @@
 #include "macroConst.h"
 #include "globalVar.h"
 
-// Display
-void dec_2_bin (int decimal, int *temp_arr) {  //bxxxx: (0 -> 9)
+void dec_2_bin (int decimal, bool *temp_arr) {  //bxxxx: (0 -> 9)
   temp_arr[3] = (decimal / 1) % 2;    // LSB
   temp_arr[2] = (decimal / 2) % 2;
   temp_arr[1] = (decimal / 4) % 2;
@@ -19,16 +18,25 @@ void turn_to_ClkDisp(long dis_time, int *arr) {   // arr[0]: hr - arr[1]: min - 
   arr[1] = (dis_time / FREQUENCY / 60) % 60;
   arr[0] = dis_time / FREQUENCY / (60 * 60);
 }
-void disp(long dis_time, int dis_select, bool normal_mode = false, bool countUp_mode = false , bool countDown_mode = false) {
+
+void BCD_to_led(bool *arr) {
+   digitalWrite(BCD_A, arr[3]);
+   digitalWrite(BCD_B, arr[2]);
+   digitalWrite(BCD_C, arr[1]);
+   digitalWrite(BCD_D, arr[0]);
+}
+
+// Display
+void disp(long dis_time, byte dis_select, bool normal_mode = false, bool countUp_mode = false , bool countDown_mode = false) {
   int dis_clk[4];
   int first_num, second_num, third_num, fourth_num;
-  int first_num_bin[2], second_num_bin[2], third_num_bin[2], fourth_num_bin[2];
+  bool first_num_bin[4], second_num_bin[4], third_num_bin[4], fourth_num_bin[4];
   turn_to_ClkDisp(dis_time, dis_clk);
   // Convert dis_clk to BCD num
-  first_num = (normal_mode) ? dis_clk[0] / 10 : (countUp_mode) ? dis_clk[2] / 10 : (countDown_mode) ? dis_clk[1] / 10 : first_num;
-  second_num = (normal_mode) ? dis_clk[0] % 10 : (countUp_mode) ? dis_clk[2] % 10 : (countDown_mode) ? dis_clk[1] % 10 : second_num;
-  third_num = (normal_mode) ? dis_clk[1] / 10 : (countUp_mode) ? dis_clk[3] / 10 : (countDown_mode) ? dis_clk[2] / 10 : third_num;
-  fourth_num = (normal_mode) ? dis_clk[1] % 10 : (countUp_mode) ? dis_clk[3] % 10 : (countDown_mode) ? dis_clk[2] % 10 : third_num;
+  first_num   = (normal_mode) ? dis_clk[0] / 10 : (countUp_mode) ? dis_clk[2] / 10 : (countDown_mode) ? dis_clk[1] / 10 : first_num;
+  second_num  = (normal_mode) ? dis_clk[0] % 10 : (countUp_mode) ? dis_clk[2] % 10 : (countDown_mode) ? dis_clk[1] % 10 : second_num;
+  third_num   = (normal_mode) ? dis_clk[1] / 10 : (countUp_mode) ? dis_clk[3] / 10 : (countDown_mode) ? dis_clk[2] / 10 : third_num;
+  fourth_num  = (normal_mode) ? dis_clk[1] % 10 : (countUp_mode) ? dis_clk[3] % 10 : (countDown_mode) ? dis_clk[2] % 10 : third_num;
   
   Serial.print("Time:\t");
   Serial.print(first_num);
@@ -42,26 +50,41 @@ void disp(long dis_time, int dis_select, bool normal_mode = false, bool countUp_
   Serial.println(select);
   
   dec_2_bin(first_num, first_num_bin);
-  // Error XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-  second_num_bin[3] = (second_num / 1) % 2;    // LSB
-  second_num_bin[2] = (second_num / 2) % 2;
-  second_num_bin[1] = (second_num / 4) % 2;
-  second_num_bin[0] = (second_num / 8) % 2;    // MSB
-//  dec_2_bin(second_num, second_num_bin);     // Error
-  
-  third_num_bin[3] = (third_num / 1) % 2;    // LSB
-  third_num_bin[2] = (third_num / 2) % 2;    
-  third_num_bin[1] = (third_num / 4) % 2;
-  third_num_bin[0] = (third_num / 8) % 2;    // MSB
-//  dec_2_bin(third_num, third_num_bin);     // Error
+  dec_2_bin(second_num, second_num_bin);
+  dec_2_bin(third_num, third_num_bin);     
+  dec_2_bin(fourth_num, fourth_num_bin);    
 
-  fourth_num_bin[3] = (fourth_num / 1) % 2;    // LSB
-  fourth_num_bin[2] = (fourth_num / 2) % 2;
-  fourth_num_bin[1] = (fourth_num / 4) % 2;
-  fourth_num_bin[0] = (fourth_num / 8) % 2;    // MSB
-//  dec_2_bin(fourth_num, fourth_num_bin);     // Error
-  // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
+  switch (led_num) {
+    case 1:                                    // X- : --
+      BCD_to_led(first_num_bin);
+      digitalWrite(FIRST_NUM, HIGH);
+      digitalWrite(SECOND_NUM, LOW);
+      digitalWrite(THIRD_NUM, LOW);
+      digitalWrite(FOURTH_NUM, LOW);
+      break;
+    case 2: 
+      BCD_to_led(second_num_bin);
+      digitalWrite(FIRST_NUM, LOW);
+      digitalWrite(SECOND_NUM, HIGH);
+      digitalWrite(THIRD_NUM, LOW);
+      digitalWrite(FOURTH_NUM, LOW);
+      break;
+    case 3:
+      BCD_to_led(third_num_bin);
+      digitalWrite(FIRST_NUM, LOW);
+      digitalWrite(SECOND_NUM, LOW);
+      digitalWrite(THIRD_NUM, HIGH);
+      digitalWrite(FOURTH_NUM, LOW);
+      break;
+    case 4:
+      BCD_to_led(fourth_num_bin);
+      digitalWrite(FIRST_NUM, LOW);
+      digitalWrite(SECOND_NUM, LOW);
+      digitalWrite(THIRD_NUM, LOW);
+      digitalWrite(FOURTH_NUM, HIGH);
+      break;
+  }
 //
 //  // U1 //////////////////////////////////////////
 //  digitalWrite(FIRST_DISP_A, first_num_bin[3]);
