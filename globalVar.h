fef95b447506528835cd13d0ee5201cc20bc6ab0
ATFox <99324602+atfox272@users.noreply.github.com>
2022-12-10 09:54:45 +0700

    Update globalVar.h

--- globalVar.h
+++ globalVar.h
@@ -5,7 +5,8 @@
 #include "macroConst.h"
 
 // External Interrupt type
-bool exINT_type = (INT_PIN == 2) ? 0 : 1; // Select INT 0
+bool exINT_mode = (MODE_PIN == 2) ? 0 : 1; // Select INT 0
+bool exINT_set = (SET_PIN == 3) ? 1 : 0;   // Select INT 1
 
 // Time var
 volatile long count_per_025sec = 0;       // 0 -> 8639999  (00:00:00:00 -> 23:59:59:99)
@@ -19,13 +20,19 @@ volatile byte select = 0;                 // 0 -> 2
 // Adjust flag
 volatile bool inc_enable = false;         // increase enable flag
 volatile bool dec_enable = false;         // decrease enable flag
+
 volatile bool countUp_enable = false;     // 
 volatile bool countDown_enable = false;   // 
+volatile bool isCounting = false;         // isCounting = countUp_enable || countDown_enable
+volatile bool isAlarming = false;
+volatile bool turnOn_alarmMode = false;
+volatile bool hold_PLUSbutton = false;
 
 // Numbers of led
-volatile byte led_num = 1;              // 1 -> 4
+volatile byte led_num = 1;                // 1 -> 4
 volatile bool colon   = false;
 // Temp
 volatile unsigned int temp_count = 0;
+volatile unsigned int temp_count2 = 0;    // Use for turning ON / OFF alarm mode
 
 #endif
