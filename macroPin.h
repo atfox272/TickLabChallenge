8816170f54956234a46e97d9221ec07d45775983
ATFox <99324602+atfox272@users.noreply.github.com>
2022-12-10 09:55:51 +0700

    Update macroPin.h

--- macroPin.h
+++ macroPin.h
@@ -14,14 +14,16 @@
 #define FOURTH_NUM  4
 
 // Button Pin
-#define INT_PIN         2     // MODE & SET signal
+#define MODE_PIN         2    // SET signal
+#define SET_PIN          3    // SET signal
 #define MODE_ENCODE_PIN A0    // MODE signal encoder            
 #define SET_ENCODE_PIN  A1    // SET signal encoder
 #define INC_PIN         A2    // + signal
 #define DEC_PIN         A3    // - signal
 #define BUZZ_PIN        A4    // Alarm signal
+#define ALARM_STATE_PIN A5    // Alarm is ON / OFF
 
 // Clock
-#define COLON_CLK       3
+#define COLON_CLK       12
 
 #endif
