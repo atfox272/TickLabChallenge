cc6be41c3fde2db71003a69756c6b092bf6ee336
ATFox <99324602+atfox272@users.noreply.github.com>
2023-01-05 17:44:52 +0700

    Update macroPin.h

--- macroPin.h
+++ macroPin.h
@@ -1,11 +1,11 @@
-#ifndef _gvar_macro_h
+  #ifndef _gvar_macro_h
 #define _gvar_macro_h
 
 // BCD pin
-#define BCD_A 8
-#define BCD_B 9
-#define BCD_C 10
-#define BCD_D 11
+#define BCD_A A0
+#define BCD_B A1
+#define BCD_C A2
+#define BCD_D A3
 
 // Choose LED
 #define FIRST_NUM   7
@@ -14,16 +14,17 @@
 #define FOURTH_NUM  4
 
 // Button Pin
-#define MODE_PIN         2    // SET signal
-#define SET_PIN          3    // SET signal
-#define MODE_ENCODE_PIN A0    // MODE signal encoder            
-#define SET_ENCODE_PIN  A1    // SET signal encoder
-#define INC_PIN         A2    // + signal
-#define DEC_PIN         A3    // - signal
+#define COM_INT         8     // Input Capture TC1 D8 pin
+#define MODE_ENCODER    9     // MODE signal (encoder)
+#define SET_ENCODER     11    // SET signal (encoder)
+#define INC_ENCODER     10    // INC signal (encoder)
+#define DEC_ENCODER     12    // DEC signal (encoder)
+
+
 #define BUZZ_PIN        A4    // Alarm signal
 #define ALARM_STATE_PIN A5    // Alarm is ON / OFF
 
 // Clock
-#define COLON_CLK       12
+#define COLON_CLK       3
 
 #endif
